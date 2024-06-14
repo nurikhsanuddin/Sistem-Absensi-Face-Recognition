e7249c97c1bd929a80681be5386dd331f4fe5834
nurikhsanuddin <m.nurikhsanuddin@gmail.com>
2024-06-15 05:06:57 +0700

    Initial commit

---
+++ flask_db.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 14, 2024 at 10:05 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flask_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id` int NOT NULL,
+  `person` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `skill` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` varchar(100) NOT NULL,
+  `latitude` varchar(100) DEFAULT NULL,
+  `longitude` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accs_hist`
+--
+
+CREATE TABLE `accs_hist` (
+  `accs_id` int NOT NULL,
+  `accs_date` date NOT NULL,
+  `accs_prsn` varchar(3) NOT NULL,
+  `accs_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `img_dataset`
+--
+
+CREATE TABLE `img_dataset` (
+  `img_id` int NOT NULL,
+  `img_person` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prs_mstr`
+--
+
+CREATE TABLE `prs_mstr` (
+  `prs_nbr` varchar(3) NOT NULL,
+  `prs_name` varchar(50) NOT NULL,
+  `prs_skill` varchar(30) NOT NULL,
+  `prs_active` varchar(1) NOT NULL DEFAULT 'Y',
+  `prs_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `accs_hist`
+--
+ALTER TABLE `accs_hist`
+  ADD PRIMARY KEY (`accs_id`),
+  ADD KEY `accs_date` (`accs_date`);
+
+--
+-- Indexes for table `img_dataset`
+--
+ALTER TABLE `img_dataset`
+  ADD PRIMARY KEY (`img_id`);
+
+--
+-- Indexes for table `prs_mstr`
+--
+ALTER TABLE `prs_mstr`
+  ADD PRIMARY KEY (`prs_nbr`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `accs_hist`
+--
+ALTER TABLE `accs_hist`
+  MODIFY `accs_id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
